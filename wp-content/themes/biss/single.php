482b2da0e980f0e3188eb5b7024a3ca4694d4a60
ariniastari <astariarini@gmail.com>
2014-03-23 21:57:13 +0700

    page for single post (in use for tab ajax)

---
+++ wp-content/themes/biss/single.php
@@ -0,0 +1,27 @@
+<div class="width100">
+	<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<p><?php the_field('subheading_post'); ?></p>
+		</div>
+	</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 padnol">
+			<img class="long_thumb" src="<?php the_field('banner_post') ?>" />		
+		</div>
+		<div class="col-md-12">
+			<?php if( have_rows('content_items_post') ): ?>
+				<?php while( have_rows('content_items_post') ): the_row(); 
+					$header = get_sub_field('header_content_item');
+					$description = get_sub_field('description_content_item'); ?>
+					<h3><?php echo $header ?></h3>
+					<?php echo $description ?>
+					<?php $i++ ?>
+				<?php endwhile; ?>
+			<?php endif; ?>	
+		</div>				
+	</div>
+</div>

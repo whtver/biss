f020d573385a6a117408e403f02fa9abe360f505
ariniastari <astariarini@gmail.com>
2014-04-17 23:01:43 +0700

    disable auto load more on scroll

--- wp-content/themes/biss/page-templates/archive.php
+++ wp-content/themes/biss/page-templates/archive.php
@@ -9,7 +9,7 @@ Template Name: archive
 	<!-- AJAX LOAD MORE -->
 	<section id="ajax-load-more" class="container martop30">
 		<div id="post">
-		<ul class="listing" data-path="<?php echo get_template_directory_uri(); ?>/ajax-load-more" data-post-type="post" data-category="news" data-taxonomy="" data-tag="" data-display-posts="2" data-scroll="true" data-button-text="LOAD MORE">
+		<ul class="listing" data-path="<?php echo get_template_directory_uri(); ?>/ajax-load-more" data-post-type="post" data-category="news" data-taxonomy="" data-tag="" data-display-posts="2" data-scroll="false" data-button-text="LOAD MORE">
 		<!-- Load Ajax Posts Here -->
 		</ul>
 		</div>
